075bf2147ec783599e4b2696d646b0438b4dd2da
Gaboarias <garias1989@gmail.com>
2026-05-30 11:11:10 -0600

    fix(redesign): AppShell sin bg-background (tapaba la aurora)

--- src/components/layout/AppShell.tsx
+++ src/components/layout/AppShell.tsx
@@ -3,7 +3,7 @@ import { Topbar } from "./Topbar";
 
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex h-screen overflow-hidden bg-background">
+    <div className="flex h-screen overflow-hidden">
       <Sidebar />
       <div className="flex flex-1 flex-col overflow-hidden">
         <Topbar />
